3e4af383ac4c465cfab30d1c2165ff3a1e2251fd
trương17052001 <tran.quang.truong.17052001@gmail.com>
2025-01-14 23:20:13 +0700

    migrare

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->text('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->dateTime('birthday');
+            $table->integer('gender');
+            $table->integer('phone')->unique();
+            $table->integer('permission');
             $table->rememberToken();
             $table->timestamps();
         });
