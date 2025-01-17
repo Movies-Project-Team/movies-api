98c1687577b6ffeb8474d8eafb85b5b1b0b7a1e9
trương17052001 <tran.quang.truong.17052001@gmail.com>
2025-01-17 20:58:15 +0700

    seeding genres and languages 1

---
+++ database/seeders/LanguagesSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LanguagesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('movies')->insert([
+            [
+                'title' => 'Anh',
+                'code' => 'Anh',
+            ],
+            [
+                'title' => 'Canada',
+                'code' => 'Canada',
+            ],
+            [
+                'title' => 'Hàn Quốc',
+                'code' => 'Hàn Quốc',
+            ],
+            [
+                'title' => 'Hồng Kông',
+                'code' => 'Hồng Kông',
+            ],
+            [
+                'title' => 'Mỹ',
+                'code' => 'Mỹ',
+            ],
+            [
+                'title' => 'Nhật Bản',
+                'code' => 'Nhật Bản',
+            ],
+            [
+                'title' => 'Pháp',
+                'code' => 'Pháp',
+            ],
+            [
+                'title' => 'Thái Lan',
+                'code' => 'Thái Lan',
+            ],
+            [
+                'title' => 'Trung Quốc',
+                'code' => 'Trung Quốc',
+            ],
+            [
+                'title' => 'Úc',
+                'code' => 'Úc',
+            ],
+            [
+                'title' => 'Đức',
+                'code' => 'Đức',
+            ],
+            [
+                'title' => 'Khác',
+                'code' => 'Khác',
+            ],
+
+        ]);
+    }
+}
