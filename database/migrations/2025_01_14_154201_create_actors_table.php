3a54aaca379d2319c27266b6ed122cf52ff44ddd
trương17052001 <tran.quang.truong.17052001@gmail.com>
2025-01-19 21:05:05 +0700

    create model

--- database/migrations/2025_01_14_154201_create_actors_table.php
+++ database/migrations/2025_01_14_154201_create_actors_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->text('name');
             $table->text('image');
+            $table->dateTime('birthdate');
+            $table->text('bio');
             $table->timestamp('created_at')->nullable();
             $table->unsignedInteger('created_by')->nullable();
             $table->timestamp('updated_at')->nullable();
